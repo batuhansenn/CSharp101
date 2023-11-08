400c8b0d66069ec9730c4cd8a0ac072dd7828d62
Batuhan Şen <59296529+batuhansenn@users.noreply.github.com>
2023-11-08 20:03:13 +0300

    Add files via upload

---
+++ Pratikler/tip_donusumleri/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class tip_donusumleri
+{
+    static void Main()
+    {
+        // Implicit Conversion(Bilinçsiz Dönüşüm)
+
+        byte a = 5;
+        sbyte b = 30;
+        short c = 10;
+
+        int d = a + b + c;
+        Console.WriteLine("d: " + d);
+
+        long h = d;
+        Console.WriteLine("h: " + h);
+
+        float i = h;
+        Console.WriteLine("i: " + i);
+
+        string e = "Batuhan";
+        char f = 'E';
+
+        object g = e + f + d;
+        Console.WriteLine("g: " + g);
+
+
+
+        // Explicit Conversion(Bilinçli Dönüşüm)
+         
+        int x = 4;
+        byte y = (byte)x;
+        Console.WriteLine("y: " + y);
+
+        int z = 100;
+        byte t = (byte)z;
+        Console.WriteLine("t: " + t);
+
+        float w = 10.3f;
+        byte v = (byte)w;
+        Console.WriteLine("v: " + v);
+        
+    }
+}
